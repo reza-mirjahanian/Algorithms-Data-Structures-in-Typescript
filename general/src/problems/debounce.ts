38f5e4a505e0a7bd243fe71cc3ca8bab521b5460
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-07-21 22:37:11 +0430

    Debounce

---
+++ general/src/problems/debounce.ts
@@ -0,0 +1,22 @@
+import internal from "stream";
+
+console.log("###-- Debounce --###");
+
+
+function debounce(fn: Function, time: number, context: void) {
+  let timeout: ReturnType < typeof setTimeout > ;
+  let callbackArgs: typeof arguments;
+
+  return function() {
+    callbackArgs = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(fn.bind(context, ...Array.from(callbackArgs), time));
+  }
+}
+
+function run() {
+  console.log('print me');
+}
+const testDebounce = debounce(run, 1);
+testDebounce();
+testDebounce();
