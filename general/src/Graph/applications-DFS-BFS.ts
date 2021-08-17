bba5d6393642560852edb329d811af520cce903e
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-08-17 18:30:52 +0430

    directed Graph

---
+++ general/src/Graph/applications-DFS-BFS.ts
@@ -0,0 +1,20 @@
+// DFS
+// 1) Detecting cycle in a graph
+// 2) Path Finding
+// 3) Topological Sorting
+// 4) To test if a graph is bipartite
+// 5) Finding Strongly Connected Components of a graph
+// 6) Solving puzzles with only one solution
+
+// BFS
+// 1) Shortest Path and Minimum Spanning Tree for unweighted graph
+// 2) Peer to Peer Networks
+// 3) Crawlers in Search Engines
+// 4) Social Networking Websites
+// 5) GPS Navigation systems:
+// 6) Broadcasting in Network:
+// 7) In Garbage Collection:
+// 8) Cycle detection in undirected graph
+// 9) Ford–Fulkerson algorithm
+// 10) To test if a graph is Bipartite
+// 11) Path Finding
