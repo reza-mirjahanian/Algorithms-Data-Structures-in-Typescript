d3c4b5c49c73fb3debef52a4c0aabc23f43976e5
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-08-17 18:01:12 +0430

    directed Graph

--- general/src/Graph/BFS.ts
+++ general/src/Graph/BFS.ts
@@ -1,74 +1,77 @@
-class Graph {
-  private vertices: string[];
-  private adjacent: Record < string, string[] > ;
-  private edges: number;
-  constructor() {
-    this.vertices = [];
-    this.adjacent = {};
-    this.edges = 0;
-  }
-
-  addVertex(v: string) {
-    this.vertices.push(v);
-    this.adjacent[v] = [];
-  }
-
-  addEdge(v: string, w: string) {
-    this.adjacent[v].push(w);
-    this.adjacent[w].push(v);
-    this.edges++;
-  }
-  neighbors(v: string) {
-    return this.adjacent[v];
-  }
-}
-
-const g = new Graph();
-
-g.addVertex("1");
-g.addVertex("2");
-g.addVertex("3");
-g.addVertex("4");
-g.addVertex("5");
-g.addVertex("6");
-
-
-g.addEdge("1", "2");
-g.addEdge("1", "3");
-g.addEdge("3", "5");
-g.addEdge("2", "5");
-g.addEdge("2", "4");
-g.addEdge("4", "5");
-g.addEdge("4", "6");
-g.addEdge("5", "6");
-
-
-console.log(g.neighbors("1"))
-console.log(g.neighbors("2"))
-console.log(g.neighbors("3"))
-console.log(g.neighbors("4"))
-console.log(g.neighbors("5"))
-console.log(g.neighbors("6"))
-
-function graphBFS(graph: Graph, start: string) {
-  const queue = [start],
-    visited = new Set(queue);
-  while (queue.length) {
-    const shifted = queue.shift();
-    console.log({
-      shifted
-    })
-    if (shifted) {
-      for (const item of graph.neighbors(shifted)) {
-        if (!visited.has(item)) {
-          queue.push(item);
-          visited.add(item);
+namespace BFS{
+    class Graph {
+        private vertices: string[];
+        private adjacent: Record < string, string[] > ;
+        private edges: number;
+        constructor() {
+            this.vertices = [];
+            this.adjacent = {};
+            this.edges = 0;
         }
-      }
+
+        addVertex(v: string) {
+            this.vertices.push(v);
+            this.adjacent[v] = [];
+        }
+
+        addEdge(v: string, w: string) {
+            this.adjacent[v].push(w);
+            this.adjacent[w].push(v);
+            this.edges++;
+        }
+        neighbors(v: string) {
+            return this.adjacent[v];
+        }
     }
 
-  }
+    const g = new Graph();
+
+    g.addVertex("1");
+    g.addVertex("2");
+    g.addVertex("3");
+    g.addVertex("4");
+    g.addVertex("5");
+    g.addVertex("6");
+
+
+    g.addEdge("1", "2");
+    g.addEdge("1", "3");
+    g.addEdge("3", "5");
+    g.addEdge("2", "5");
+    g.addEdge("2", "4");
+    g.addEdge("4", "5");
+    g.addEdge("4", "6");
+    g.addEdge("5", "6");
+
+
+    console.log(g.neighbors("1"))
+    console.log(g.neighbors("2"))
+    console.log(g.neighbors("3"))
+    console.log(g.neighbors("4"))
+    console.log(g.neighbors("5"))
+    console.log(g.neighbors("6"))
+
+    function graphBFS(graph: Graph, start: string) {
+        const queue = [start],
+            visited = new Set(queue);
+        while (queue.length) {
+            const shifted = queue.shift();
+            console.log({
+                shifted
+            })
+            if (shifted) {
+                for (const item of graph.neighbors(shifted)) {
+                    if (!visited.has(item)) {
+                        queue.push(item);
+                        visited.add(item);
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    graphBFS(g, "1");
+
 }
-
-
-graphBFS(g, "1");
---
+++ general/src/Graph/DFS.ts
@@ -0,0 +1,52 @@
+class Graph {
+
+  private adj: number[][];
+  constructor(private V: number) {
+    this.adj = new Array(V).fill([]);
+  }
+
+  AddEdge(v: number, w: number) {
+    this.adj[v].push(w); // Add w to v's list.
+  }
+
+  DFSUtil(v: number, visited : boolean[]) {
+    visited[v] = true;
+    console.log(v + " ");
+
+    // Recur for all the
+    // vertices adjacent to this
+    // vertex
+    for (const n of this.adj[v]) {
+      if (!visited[n]) this.DFSUtil(n, visited);
+    }
+  }
+
+
+
+  //disconnected
+  DFS() {
+
+    const visited = new Array(this.V).fill(false);
+
+
+    // all vertices one by one
+    for (let i = 0; i < this.V; ++i)
+      if (visited[i] == false) {
+        this.DFSUtil(i, visited);
+      }
+  }
+}
+
+
+const g = new Graph(4);
+
+g.AddEdge(0, 1);
+g.AddEdge(0, 2);
+g.AddEdge(1, 2);
+g.AddEdge(2, 0);
+g.AddEdge(2, 3);
+g.AddEdge(3, 3);
+
+
+
+g.DFS();
