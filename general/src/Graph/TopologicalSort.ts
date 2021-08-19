a472b777cea3bd6ccae1f723e281c43f36fcc29b
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-08-19 11:54:44 +0430

    Bipartite

--- general/src/Graph/TopologicalSort.ts
+++ general/src/Graph/TopologicalSort.ts
@@ -5,6 +5,8 @@
 
 // We can modify DFS to find Topological Sorting of a graph. In DFS, we start from a vertex, we first print it and then recursively call DFS for its adjacent vertices. In topological sorting, we use a temporary stack. We don’t print the vertex immediately, we first recursively call topological sorting for all its adjacent vertices, then push it to a stack. Finally, print contents of the stack. Note that a vertex is pushed to stack only when all of its adjacent vertices (and their adjacent vertices and so on) are already in the stack.
 
+
+// Step 1: Identify vertices that have no incoming edge (in-degree 0)
 class GraphTopo {
 
   private V: number; // No. of vertices
