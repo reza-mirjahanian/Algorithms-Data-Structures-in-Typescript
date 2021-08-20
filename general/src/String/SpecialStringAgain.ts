dbad5a586bc07895b2fe56508bfa638d7aef4104
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-08-20 10:58:41 +0430

    SpecialStringAgain.ts

--- general/src/String/SpecialStringAgain.ts
+++ general/src/String/SpecialStringAgain.ts
@@ -1,59 +1,41 @@
 function substrCount(n: number, s: string) {
 
+  let count = n; // each char counts one
+  let iSpecialCnt = 0,
+    iCurrCount = 0,
+    iPrevCount = 0,
+    iPrevPrevCount = 0;
 
-  // store count of special Palindromic substring
-  let result = 0;
+  for (let i = 1; i < n; i++) {
+    let pre = s[i - 1];
+    let current = s[i];
 
-  // it will store the count of continues same char
-  let sameChar = [...Array(n)];
+    if (pre === current) {
+      iCurrCount++;
+      count += iCurrCount;
 
-  let i = 0;
-
-  // traverse string character from left to right
-  while (i < n) {
-    // store same character count
-    let sameCharCount = 1;
-
-    let j = i + 1;
-
-    // count smiler character
-    while (s[i] == s[j] && j < n) {
-      sameCharCount++;
-      j++;
+      if (iSpecialCnt > 0) {
+        iSpecialCnt--;
+        count++;
+      }
+    } else {
+      iCurrCount = 0;
+      if (i > 1 && (s[i - 2] == current)) {
+        iSpecialCnt = iPrevPrevCount;
+        count++;
+      } else {
+        iSpecialCnt = 0;
+      }
     }
 
-    // Case : 1
-    // so total number of substring that we can
-    // generate are : K *( K + 1 ) / 2
-    // here K is sameCharCount
-    result += (sameCharCount * (sameCharCount + 1)) / 2;
+    if (i > 1) {
+      iPrevPrevCount = iPrevCount;
+    }
 
-    // store current same char count in sameChar[]
-    // array
-    sameChar[i] = sameCharCount;
+    iPrevCount = iCurrCount;
+  } //
 
-    // increment i
-    i = j;
-  }
-
-  // Case 2: Count all odd length Special Palindromic
-  // substring
-  for (let j = 1; j < n; j++) {
-    // if current character is equal to previous
-    // one then we assign Previous same character
-    // count to current one
-    if (s[j] == s[j - 1]) sameChar[j] = sameChar[j - 1];
-
-    // case 2: odd length
-    if (
-      j > 0 &&
-      j < n - 1 &&
-      s[j - 1] == s[j + 1] &&
-      s[j] != s[j - 1]
-    )
-      result += Math.min(sameChar[j - 1], sameChar[j + 1]);
-  }
-
-  // subtract all single length substring
-  return result - n;
+  return count;
 }
+
+console.log(substrCount(5, 'aasaa'))
